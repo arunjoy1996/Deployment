e1fb80bd968504bb6192ec163d3b6d60df700584
arunjoy1996 <22f1001470@ds.study.iitm.ac.in>
2026-06-12 17:42:15 +0530

    added absolute path in model_loader.py

--- api/model_loader.py
+++ api/model_loader.py
@@ -1,7 +1,10 @@
 import joblib
 import os
 
+# MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "model", "rf_model.joblib")
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "model", "rf_model.joblib")
+MODEL_PATH = os.path.abspath(MODEL_PATH)  # Convert to absolute path
+
 model = None
 if os.path.exists(MODEL_PATH):
 	model = joblib.load(MODEL_PATH)
